ff024afaf5126446cfd8ca8ef182c1892d559029
Xinnong Wang <xinnong.wang@oracle.com>
2019-03-26 23:12:51 +0800

    Fix for wercker pipeline

--- ci/setBuildEnv.sh
+++ ci/setBuildEnv.sh
@@ -3,5 +3,5 @@
 homeDir=$(cd $(dirname ${BASH_SOURCE[0]})/.. > /dev/null; pwd -P)
 
 export GOPATH=$homeDir
-mkdir -p $GOPATH/src/gitlab-odx.oracledx.com/wcai
+mkdir -p $GOPATH/src/github.com/oracle
 ln -s $homeDir $GOPATH/src/github.com/oracle/speedle
