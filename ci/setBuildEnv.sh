4976b616db54037b5314bee554810a2454e80b3a
jiefu <jiexiang.fu@oracle.com>
2019-04-22 15:58:41 -0400

    link /go/bin to wercker/output.

--- ci/setBuildEnv.sh
+++ ci/setBuildEnv.sh
@@ -2,6 +2,9 @@
 
 homeDir=$(cd $(dirname ${BASH_SOURCE[0]})/.. > /dev/null; pwd -P)
 
-export GOPATH=$homeDir
+rm /go/bin
+ln -s ${WERCKER_OUTPUT_DIR} /go/bin
+
+#export GOPATH=$homeDir
 #mkdir -p $GOPATH/src/github.com/oracle
 #ln -s $homeDir $GOPATH/src/github.com/oracle/speedle
