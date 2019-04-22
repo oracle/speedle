d3826ce656ac0a44fbf6051d2958c5ba4e28c122
jiefu <jiexiang.fu@oracle.com>
2019-04-22 13:06:19 -0400

    no need to set env.

--- ci/setBuildEnv.sh
+++ ci/setBuildEnv.sh
@@ -2,6 +2,6 @@
 
 homeDir=$(cd $(dirname ${BASH_SOURCE[0]})/.. > /dev/null; pwd -P)
 
-export GOPATH=$homeDir
-mkdir -p $GOPATH/src/github.com/oracle
-ln -s $homeDir $GOPATH/src/github.com/oracle/speedle
+#export GOPATH=$homeDir
+#mkdir -p $GOPATH/src/github.com/oracle
+#ln -s $homeDir $GOPATH/src/github.com/oracle/speedle
